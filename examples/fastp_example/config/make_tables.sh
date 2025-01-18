124a1b6c74f80076c9f645a7c2c0ff2e126a5fc3
Paul Magwene <paul.magwene@duke.edu>
2025-01-17 22:23:23 -0500

    minor fixes to examples

--- examples/fastp_example/config/make_tables.sh
+++ examples/fastp_example/config/make_tables.sh
@@ -4,5 +4,5 @@
 # sample,fq1,fq2
 # SampleName,../rel/path/to/fq_R1.fastq.gz,../rel/path/to/fq_R2.fastq.gz,../path/to/refgenome.fa
 
-echo sample,fq1,fq2,refgenome > fastp_table.csv
+echo sample,fq1,fq2 > fastp_table.csv
 parallel echo {},reads/{}_R1.fastq.gz,reads/{}_R2.fastq.gz :::: sample_names.txt >> fastp_table.csv
